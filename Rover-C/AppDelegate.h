3e43713a7c9fe4042a061c97ffd5a7ed5f3eb941
Sohailbasha <shlbsh94@gmail.com>
2018-07-28 16:12:22 -0400

    Initial Commit

---
+++ Rover-C/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Rover-C
+//
+//  Created by Ilias Basha on 7/28/18.
+//  Copyright © 2018 sohail. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Rover-C/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Rover-C
+//
+//  Created by Ilias Basha on 7/28/18.
+//  Copyright © 2018 sohail. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
